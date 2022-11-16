86e010db8464630ad8fe87a1c8aa482634c8f724
Giorgio Villani <15069760+giorgio-villani@users.noreply.github.com>
2022-11-16 16:22:33 +0000

    adjusted install_ta-lib.sh

--- scripts/install_ta-lib.sh
+++ scripts/install_ta-lib.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 
+parse_url() {
+    if [ -z $LINODE_ID]; then
+        echo "${1##*/}"
+    else
+        echo "${1##*/}"
+    fi
+}
+
 url="http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz"
-target="/home/$USERNAME/Downloads/$(parse_url $url)"
-sudo -u $USERNAME mkdir /home/$USERNAME/Downloads
+target_directory="/home/$USERNAME/Downloads"
+target="$target_directory/$(parse_url $url)"
 
-sudo -u $USERNAME curl "$url" --output "$target"
+echo $url
+echo $target_directory
+echo $target
 
+mkdir /home/$USERNAME/Downloads
+
+wget "$url" -P "$target_directory"
 sudo tar -xzf $target --directory /home/$USERNAME/
-sudo rm $target
+
+# pip3 install --upgrade pip
+# sudo apt-get install gcc
+# sudo apt-get install -y make
+
 cd /home/$USERNAME/ta-lib/
 
 sudo ./configure --prefix=/usr
 sudo make
 sudo make install
+
 cd ~
 sudo rm -rf ta-lib/
 pip install ta-lib
